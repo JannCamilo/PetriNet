6eacef172381859243a92be93babf993a174b59b
j23sanch <jann.sanchez-huertas@imt-atlantique.net>
2023-10-11 14:37:03 +0200

    feat: add the classes place, arc and transition

---
+++ src/Classes/Place.java
@@ -0,0 +1,17 @@
+package Classes;
+
+import java.util.List;
+
+public class Place {
+	
+	private int nbTokens;
+	
+	public Place(int nbTokens, List<Arc> listArcs ) {}	
+	public Place(int nbTokens) {}
+	
+	public int getNbTOkens() {return this.nbTokens;}
+	public void setNbTokens() {}
+	public List<Arc> getArcs() {return null;}
+	public void setArcs(List<Arc> listArcs) {}
+	public String toString() {return null;}
+}
--- src/Interfaces/IPetriNetwork.java
+++ src/Interfaces/IPetriNetwork.java
@@ -1,5 +1,17 @@
 package Interfaces;
 
-public interface IPetriNetwork {
+import Classes.Arc;
+import Classes.Place;
+import Classes.Transition;
 
+public interface IPetriNetwork {	
+	
+	public void fireAll();
+	public void addPlace(Place place);
+	public void addArc(Arc arc);
+	public void addTransition(Transition transition);
+	public void deletePlace(Place place);
+	public void deleteArc(Arc arc);
+	public void deleTransition(Transition transition);
+	public void fireTransition(Transition transition);
 }
