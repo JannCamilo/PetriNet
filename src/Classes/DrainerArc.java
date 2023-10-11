1ef785ec228f1378ad50e39be39d782740125421
j23sanch <jann.sanchez-huertas@imt-atlantique.net>
2023-10-11 15:08:24 +0200

    feat: add the sub-class ZeroArc and DrainerArc

---
+++ src/Classes/DrainerArc.java
@@ -0,0 +1,13 @@
+package Classes;
+
+public class DrainerArc extends Arc{
+
+	public DrainerArc(Place place, Transition transition) {
+		super(place, transition);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public boolean isActive() {return false;}	
+	public void updatePlace() {}
+
+}
---
+++ src/Classes/ZeroArc.java
@@ -0,0 +1,12 @@
+package Classes;
+
+public class ZeroArc extends Arc{
+
+	public ZeroArc(Place place, Transition transition) {
+		super(place, transition);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public boolean isActive(){return false;}
+
+}
