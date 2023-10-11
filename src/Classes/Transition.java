4bf5bcdef48d81e2badba701baf88a06a37a15c2
Felipe LOBATO <felipelobato99@poli.ufrj.br>
2023-10-11 14:52:37 +0200

    feat: edited class Transition

--- src/Classes/Transition.java
+++ src/Classes/Transition.java
@@ -1,5 +1,18 @@
 package Classes;
 
-public class Transition {
+import java.util.List;
 
+public class Transition {
+	
+	public Transition(List<Arc> listArcs) {};
+	public Transition() {};
+	
+	public boolean fireable() {return false;}
+	public void fire() {}
+	
+	public List<Arc> getArcs() {return null;}
+	public void setArcs(List<Arc> listArcs) {}
+	public void addArc(Arc arc) {}
+	public void deleteArc (Arc arc) {}
+	public String toString() {return null;}
 }
