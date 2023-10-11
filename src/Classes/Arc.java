3bbcc1062d134c5a83ce7d98b34f4db786d8e054
j23sanch <jann.sanchez-huertas@imt-atlantique.net>
2023-10-11 15:00:30 +0200

    feat: edited the classes Arc

--- src/Classes/Arc.java
+++ src/Classes/Arc.java
@@ -1,5 +1,17 @@
 package Classes;
 
 public class Arc {
-
+	
+	public Arc(Place place, Transition transition) {}
+	
+	
+	public Place getPlace(Place place) {return place;}
+	public void setPlace(Place place) {}
+	public Transition getTransition() {return null;}
+	public void setTransition(Transition transition) {}
+	public boolean isActivate() {return false;}
+	public boolean isOutgoing() {return false;}
+	public String toString() {return null;}
+	public void updatePlace() {}	
+	
 }
